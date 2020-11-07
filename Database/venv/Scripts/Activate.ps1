610a21b29a79894c6ae49867533bf39bd4f08f3e
Daniel Vogel <dididerdenkeryt@outlook.de>
2020-11-07 11:29:18 +0100

    Updated virtual environment.

--- Database/venv/Scripts/Activate.ps1
+++ Database/venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Temp\ATS\Database\Code\venv"
+$env:VIRTUAL_ENV="C:\Users\didid\GitHub-Respository\AutomaticTextSummarization\Database\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- Database/venv/Scripts/activate.bat
+++ Database/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Temp\ATS\Database\Code\venv
+set VIRTUAL_ENV=C:\Users\didid\GitHub-Respository\AutomaticTextSummarization\Database\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
