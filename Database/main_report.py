e6d281c389ec323f8a45bb619cffabaec2bd59a4
Daniel Vogel <dididerdenkeryt@outlook.de>
2020-11-10 15:51:30 +0100

    Added some functions to the main report.

--- Database/main_report.py
+++ Database/main_report.py
@@ -1,6 +1,7 @@
 # Imports
 import glob
 import os
+import pandas as pd
 import collections
 
 
@@ -12,30 +13,39 @@ META_FILES = "\\\\NAS-SYSTEM\\home\\CloudStation\\Drive\\Server [Daniel]\\Active
 # Methods
 def read_files():
     global TEXT_FILES
+    global META_FILES
 
-    os.chdir(TEXT_FILES)
-    files = glob.glob("*.txt")
+    os.chdir(META_FILES)
+    meta_files = glob.glob("*.xlsx")
+    text_files = []
 
-    return files
+    for file in meta_files:
+        df = pd.read_excel(file)
+
+        for id in df["ID"]:
+            text_files.append(TEXT_FILES + id + ".txt")
+
+    print(f"\tNumber of meta-files: {len(meta_files)}")
+    print(f"\tNumber of text-files: {len(text_files)}")
+
+    return text_files
 
 
-def get_corpus(files):
-    global TEXT_FILES
+def read_text(files):
+    corpus = []
 
-    corpus = [read_text(TEXT_FILES + file) for file in files]
-    print(f"Anzahl der Textdateien: {len(corpus)}")
-    print(corpus[:3])
+    for file in files:
+        try:
+            with open(file, "r", encoding="utf8", errors="ignore") as f:
+                lines = f.readlines()
+                text = "\n".join(lines)
+                text = clean_text(text)
+                corpus.append(text)
+
+        except Exception as e:
+            print(e)
 
     return corpus
-
-
-def read_text(path):
-    with open(path, "r", encoding="utf8", errors="ignore") as f:
-        lines = f.readlines()
-        text = "\n".join(lines)
-        text = clean_text(text)
-
-    return text
 
 
 def clean_text(text):
@@ -54,45 +64,89 @@ def clean_text(text):
 def get_sector_distribution():
     global META_FILES
 
-    # TODO: Use meta-files to generate a sector distribution, export graphics
+    os.chdir(META_FILES)
+    files = glob.glob("*.xlsx")
+
+    distribution = {"data_open_legal": 0,
+                    "data_wikipedia": 0,
+                    "data_corpus": 0,
+                    "data_enron": 0,
+                    "data_mendeley": 0}
+
+    for file in files:
+        df = pd.read_excel(file)
+        size = len(df)
+
+        distribution["data_open_legal"] += size if "data_open_legal" in file else 0
+        distribution["data_wikipedia"] += size if "data_wikipedia" in file else 0
+        distribution["data_corpus"] += size if "data_corpus" in file else 0
+        distribution["data_enron"] += size if "data_enron" in file else 0
+        distribution["data_mendeley"] += size if "data_mendeley" in file else 0
+
+    # TODO: Export graphics, add total text-file-count
+    print(f"\tDistribution of sectors: {distribution}")
+
+
+def get_text_length_distribution(corpus):
     distribution = {}
-    print(f"Verteilung der Datenquellen: {distribution}")
 
+    for file in corpus:
+        size = len(file)
 
-def get_document_lengths(corpus):
-    # TODO: Build dictionary of document lengths and their frequencies, export graphics
-    distribution =  {}
-    print(f"Verteilung der Textlänge: {distribution}")
+        if size in distribution:
+            distribution[size] += 1
+
+        else:
+            distribution[size] = 1
+
+    distribution = sorted(distribution.items(), key=lambda kv: (kv[1], kv[0]))
+
+    # TODO: Export graphic, search for stacked (value-grouped) charts
+    print(f"\tDistribution of text lengths: {distribution}")
 
 
 def get_words(corpus):
-    # TODO: Extract all words in a non-distinct style from all documents and append each word to the list
     words = []
+
+    # TODO: Extract all words in a non-distinct style from all documents and append each word to the list
 
     return words
 
 
 def get_word_distribution(corpus):
-    # TODO: Setup a vocabulary, export graphics
     words = get_words(corpus)
     mcw = collections.Counter(words).most_common(100)
-    print(f"Wort-Verteilung: {mcw}")
+
+    # TODO: Setup a vocabulary, export graphics
+    print(f"\tDistribution of words: {mcw}")
 
 
 def get_n_grams(corpus, n):
-    # TODO: Extract all n-grams in the corpus, export head and tail as a graphic
     distribution = {}
-    print(f"Top N-Gramme: {distribution}")
+
+    # TODO: Extract all n-grams in the corpus, export head and tail as a graphic
+    print(f"\t: {distribution}")
 
 
 # Main
 def main():
+    print("Reading files...")
     files = read_files()
-    corpus = get_corpus(files)
+
+    print("Reading text...")
+    corpus = read_text(files)
+
+    print("Exporting sector distribution...")
     get_sector_distribution()
-    get_document_lengths(corpus)
-    get_word_distribution(corpus)
-    get_n_grams(corpus, 3)
+
+    print("Exporting text length distribution...")
+    get_text_length_distribution(corpus)
+
+    print("Exporting word distribution...")
+    # get_word_distribution(corpus)
+
+    print("Exporting n-gram-statistics...")
+    # get_n_grams(corpus, 3)
 
 
 if __name__ == "__main__":
--- Database/main_wikipedia.py
+++ Database/main_wikipedia.py
@@ -7,7 +7,7 @@ import uuid
 
 
 # User Input
-INITIAL_CATEGORY = "Religion"
+INITIAL_CATEGORY = "Geschichte"
 MIN_LENGTH = 1000
 LIMIT = 100000
 
